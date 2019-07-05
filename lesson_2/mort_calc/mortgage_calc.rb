6eb76e082c478267943e1c980ec27c5e394fc582
Boruch <bdakter@gmail.com>
2019-07-05 13:59:39 -0400

    refactored loops into #get_info

--- lesson_2/mort_calc/mortgage_calc.rb
+++ lesson_2/mort_calc/mortgage_calc.rb
@@ -9,38 +9,32 @@ def valid_number?(num)
   (num.is_a?(Integer) || num.is_a?(Float)) && num > 0
 end
 
-loan_amount = nil
-loop do
-  puts 'How much borrowing?: '
-  loan_amount = gets.chomp.to_i
+def get_info(message)
+  loop do
+    puts MESSAGES[message]
+    
+    input =
+      case message
+      when 'borrowing'
+        gets.chomp.to_i
+      when 'annual_rate', 'loan_years'
+        gets.chomp.to_f
+      end
 
-  break if valid_number?(loan_amount)
-  puts 'Invalid entry'
+    break input if valid_number?(input)
+    puts MESSAGES['invalid']
+  end
 end
 
-annual_rate = nil
-loop do
-  puts 'Annual rate?:'
-  annual_rate = gets.chomp.to_f
-
-  break if valid_number?(annual_rate)
-  puts 'Invalid entry'
-end
-
-loan_years = nil
-loop do
-  puts 'Years?:'
-  loan_years = gets.chomp.to_f
-
-  break if valid_number?(loan_years)
-  puts "Invalid entry"
-end
+loan_amount = get_info('borrowing')
+annual_rate = get_info('annual_rate')
+loan_years = get_info('loan_years')
 
 annual_percent = annual_rate / 100
 monthly_percent = annual_percent / 12
 loan_months = loan_years * 12
 
-monthly_payment = loan_amount * (monthly_percent /
-  (1 - (1 + monthly_percent)**-loan_months))
+monthly_payment = (loan_amount * (monthly_percent /
+  (1 - (1 + monthly_percent)**-loan_months)))
 
-puts "your monthly payment is #{monthly_payment}"
+puts"your monthly payment is #{monthly_payment}"

53b108d133f1ab1b6058ada55efff6cccf82e740
Boruch <bdakter@gmail.com>
2019-07-05 11:47:04 -0400

    populated yaml

--- lesson_2/mort_calc/mortgage_calc.rb
+++ lesson_2/mort_calc/mortgage_calc.rb
@@ -1,5 +1,8 @@
 #! /usr/bin/env ruby
 
+require 'yaml'
+MESSAGES = YAML.load_file('mort_calc.yml')
+
 puts "Calculate your mortgage payments"
 
 def valid_number?(num)
@@ -33,13 +36,11 @@ loop do
   puts "Invalid entry"
 end
 
-
 annual_percent = annual_rate / 100
 monthly_percent = annual_percent / 12
 loan_months = loan_years * 12
 
-
 monthly_payment = loan_amount * (monthly_percent /
-  (1 - (1 + monthly_percent)**(-loan_months)))
+  (1 - (1 + monthly_percent)**-loan_months))
 
 puts "your monthly payment is #{monthly_payment}"

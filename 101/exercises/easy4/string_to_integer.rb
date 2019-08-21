1f0b8204401c48dda7419cc82a654bbfcea1d44f
B. D <bdakter+gitkraken@gmail.com>
2019-08-21 16:21:40 -0400

    add logic to account for signed numbers

--- 101/exercises/easy4/string_to_integer.rb
+++ 101/exercises/easy4/string_to_integer.rb
@@ -6,11 +6,15 @@ NUMBERS = { '1' => 1, '2' => 2, '3' => 3, '4' => 4,
             'C' => 12, 'D' => 13, 'E' => 14, 'F' => 15 }.freeze
 
 def string_to_integer(str)
+  sign = str.slice!(0) if ['+', '-'].include?(str[0])
+
   running_num = 0
   str.chars.map do |c|
     integer = NUMBERS[c]
     running_num = running_num * 10 + integer
   end
+
+  running_num *= -1 if sign == '-'
   running_num
 end
 
@@ -27,6 +31,8 @@ puts string_to_integer('321') == 321
 puts string_to_integer('4321') == 4321
 puts string_to_integer('570') == 570
 puts string_to_integer('1234') == 1234
+puts string_to_integer('-570') == -570
+puts string_to_integer('+321') == 321
 
 puts hex_to_integer('21') == 33
 puts hex_to_integer('F') == 15
